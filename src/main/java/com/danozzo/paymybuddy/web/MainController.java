683d289b860ac26d6a29205c2da393d979f3fa7e
Danozzo <miraglia.daniele410@gmail.com>
2022-10-05 18:55:32 +0200

    fix

--- src/main/java/com/danozzo/paymybuddy/web/MainController.java
+++ src/main/java/com/danozzo/paymybuddy/web/MainController.java
@@ -1,17 +1,22 @@
 package com.danozzo.paymybuddy.web;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
+    private static final Logger logger = LogManager.getLogger("MainController");
 
     @GetMapping("/login")
     public String login(){
+        logger.info("get login page");
         return "login";
     }
 
     @GetMapping("/")
     public String home() {
+        logger.info("get index page");
         return "index";
     }
 
