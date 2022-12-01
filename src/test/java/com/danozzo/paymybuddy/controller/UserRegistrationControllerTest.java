e9285a14bf89dc1e5e79cf6b52d0df2202cf54f4
Danozzo <miraglia.daniele410@gmail.com>
2022-12-01 12:32:12 +0100

    test registrationCntroller ok

--- src/test/java/com/danozzo/paymybuddy/controller/UserRegistrationControllerTest.java
+++ src/test/java/com/danozzo/paymybuddy/controller/UserRegistrationControllerTest.java
@@ -4,7 +4,6 @@ import com.danozzo.paymybuddy.model.User;
 import com.danozzo.paymybuddy.service.IUserService;
 import com.danozzo.paymybuddy.web.dto.UserRegistrationDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,92 +11,106 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockServletContext;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-import javax.servlet.ServletContext;
-
+import static java.nio.file.Paths.get;
 import static org.mockito.Mockito.when;
-import static org.springframework.http.RequestEntity.post;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(SpringExtension.class)
-@WebMvcTest(UserRegistrationController.class)
-@WebAppConfiguration
+@WebMvcTest(controllers = UserRegistrationController.class)
+@EnableWebMvc
 class UserRegistrationControllerTest {
 
     @MockBean
     private IUserService userService;
 
     @Autowired
-    private BCryptPasswordEncoder passwordEncoder;
-
-    @Autowired
     private MockMvc mockMvc;
 
+
     @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @MockBean
     private WebApplicationContext webApplicationContext;
 
+//    @Autowired
+//    ObjectMapper objectMapper;
+
     @Autowired
-    ObjectMapper objectMapper;
+    private WebApplicationContext context;
+    UserRegistrationDto userRegistrationDto;
 
     @BeforeEach
-    public void setup() throws Exception {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
+    void setup() {
+        userRegistrationDto = new UserRegistrationDto("Bryan", "Marvel", "marvel@gamil.com", "12345");
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
     }
 
-//    @Test
-//    public void givenWac_whenServletContext_thenItProvidesGreetController() {
-//        ServletContext servletContext = webApplicationContext.getServletContext();
-//
-//        Assert.assertNotNull(servletContext);
-//        Assert.assertTrue(servletContext instanceof MockServletContext);
-//        Assert.assertNotNull(webApplicationContext.getBean("greetController"));
+
+    @MockBean
+    ModelAttribute modelAttribute;
+
+
+
+//    @BeforeEach
+//    public void init() {
+//        mockMvc = MockMvcBuilders
+//                .webAppContextSetup(webApplicationContext)
+//                .apply(springSecurity())
+//                .build();
 //    }
 
 
 
+//    @Test
+//    void userRegistrationDto() {
+//
+//    }
 
     @Test
-    void userRegistrationDto() {
-        
-    }
+    void showRegistrationForm() throws Exception {
+       mockMvc.perform(MockMvcRequestBuilders.get("/registration"))
+                .andDo(print())
+                .andExpect(view().name("registration")
+                );
 
-    @Test
-    void showRegistrationForm() {
+
     }
 
     @Test
     void registerUserAccount()throws Exception {
-                //Given
 
-        UserRegistrationDto user = new UserRegistrationDto("Frank", "Palumbo", "palumbo@mail.com", "12345");
-        String cryptedPassword = passwordEncoder.encode(user.getPassword());
-        //When
-        when(userService.save(user,cryptedPassword)).thenReturn(new User());
-        //Then
-
-
-        mockMvc.perform(post("redirect:/registration?success")
-                .contentType(MediaType.APPLICATION_JSON).content
+        UserRegistrationDto user =
+                new UserRegistrationDto("Frank", "Palumbo", "palumbo@mail.com", "12345");
+        String encryptedPassword = passwordEncoder.encode(user.getPassword());
+        when(userService.save(user,encryptedPassword)).thenReturn(new User());
 
 
 
-                .andDo(print()).andExpect(status().isOk())
-                .andExpect(model().size(3))
-                .andExpect(jsonPath("$.data['user'].firstName").value(user.getFirstName()))
-                .andExpect(jsonPath("$.data['user'].lastName").value(user.getLastName()))
-                .andExpect( jsonPath("$.data['account'].mail").value(user.getEmail()));
+        mockMvc.perform(MockMvcRequestBuilders.post("/registration")
+                .contentType(MediaType.APPLICATION_JSON)
+                .param("firstName", "Frank")
+                .param("lastName","Palumbo")
+                .param("email", "palum@gmail.com")
+                .param("password", "12345")
+                .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/registration?success"));
 
 
     }
-    }
+
 }
